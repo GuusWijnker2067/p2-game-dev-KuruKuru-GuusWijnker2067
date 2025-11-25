ef935ce9e64b1d95b9ccf5cf2668833a39d386bb
GuusWijnker2067 <guuswijnker206@gmail.com>
2025-11-25 15:23:04 +0100

    .

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform player;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Utilities : MonoBehaviour
+{
+    public void TestFunctie()
+    {
+        Debug.Log("De knop werkt");
+    }
+
+    
+}
